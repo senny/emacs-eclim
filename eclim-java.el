dca61f132be4cd729da2fcf00a6a42e9b45f0149
Yves Senn <yves.senn@gmail.com>
2009-08-19 15:58:22 +0200

    added commands to display the java classpath

--- eclim-java.el
+++ eclim-java.el
@@ -8,5 +8,19 @@
                                "-l" "standard"
                                "-o" (number-to-string (eclim--byte-offset)))))
 
+(defun eclim/java_classpath (project)
+  (eclim--check-project project)
+  (eclim--call-process "java_classpath" "-p" project))
+
+(defun eclim/java_classpath_variables ()
+  ;; TODO: fix trailing whitespaces
+  (mapcar (lambda (line)
+            (split-string line "-")) (eclim--call-process "java_classpath_variables")))
+
+(defun eclim/java_classpath_variable_create (name path)
+  (eclim--call-process "java_classpath_variable_create" "-n" name "-p" path))
+
+(defun eclim/java_classpath_variable_delete (name)
+  (eclim--call-process "java_classpath_variable_create" "-n" name))
 
 (provide 'eclim-java)
