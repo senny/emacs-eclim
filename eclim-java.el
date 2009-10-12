069f9240075f9ac23398f6ef6ec49505ebac5b6f
Fredrik Appelberg <fredrik.appelberg@gmail.com>
2009-10-12 21:31:16 +0200

    WIP: importing java classes

--- eclim-java.el
+++ eclim-java.el
@@ -23,6 +23,17 @@
 (defun eclim/java_classpath_variable_delete (name)
   (eclim--call-process "java_classpath_variable_create" "-n" name))
 
+(defun eclim/java-import (project pattern)
+  (eclim--check-project project)
+  (eclim--call-process "java_import"
+                       "-n" project
+                       "-p" pattern))
+
+(defun eclim/java-import-order (project)
+  (eclim--check-project project)
+  (eclim--call-process "java_import_order"
+                       "-p" project))
+
 (defun eclim/java-import-missing (project)
   (eclim--check-project project)
   (eclim--call-process "java_import_missing"
@@ -34,6 +45,25 @@
   (eclim--call-process "java_imports_unused"
                        "-p" project
                        "-f" (eclim--project-current-file)))
+
+(defun eclim--word-at-point ()
+  "Returns a string containing the word positioned under the
+  cursor. Not exactly the perfect solution for picking out java
+  tokens, but will have to do."
+  (save-excursion
+    (forward-char)
+    (backward-word 1)
+    (let ((p (point)))
+      (forward-word 1)
+      (buffer-substring-no-properties p (point)))))
+
+(defun eclim-java-import ()
+  "Reads the token at the point and calls eclim to resolve it to
+a java type that can be imported."
+  (interactive)
+  (let* ((pattern (eclim--word-at-point))
+	 (imports (eclim/java-import (eclim--project-name) pattern)))
+    imports))
 
 (defun eclim-java-import-missing ()
   (interactive)
