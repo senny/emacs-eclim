2bd7051269d8b7b8bc7491a0ca89ad2700e077f3
Fredrik Appelberg <fredrik.appelberg@gmail.com>
2009-10-14 21:14:25 +0200

    Better organizing of import statements.

--- eclim-java.el
+++ eclim-java.el
@@ -1,3 +1,4 @@
+
 (defun eclim/java-complete ()
   (mapcar (lambda (line)
             (split-string line "|"))
@@ -57,8 +58,64 @@
       (forward-word 1)
       (buffer-substring-no-properties p (point)))))
 
+(defun string-startswith-p (string prefix)
+  ;; TODO: there is probably already a library function that does this
+  (equal (substring-no-properties string 0 (string-width prefix)) prefix))
+
+(defun string-endswith-p (string prefix)
+  ;; TODO: there is probably already a library function that does this
+  (let ((w (string-width string)))
+    (equal (substring-no-properties string (- w (string-width prefix)) w) prefix)))
+
+(defun eclim--java-package-components (package)
+  "Returns the components of a Java package statement."
+  (split-string package "\\."))
+
+(defun eclim--java-wildcard-includes-p (wildcard package)
+  "Returns true if PACKAGE is included in the WILDCARD import statement."
+  (if (not (string-endswith-p wildcard ".*")) nil
+    (equal (butlast (eclim--java-package-components wildcard))
+	   (butlast (eclim--java-package-components package)))))
+
+(defun eclim--java-sort-imports (imports imports-order)
+  "Sorts a list of imports according to a given sort order, removing duplicates."
+  (flet ((sort-imports (imports-order imports result)
+		       (cond ((null imports) result)
+			     ((null imports-order)
+			      (sort-imports nil nil (append result imports)))
+			     (t 
+			      (flet ((matches-prefix (x) (string-startswith-p x (first imports-order))))
+				(sort-imports (rest imports-order)
+					      (remove-if #'matches-prefix imports)
+					      (append result (remove-if-not #'matches-prefix imports)))))))
+	 (remove-duplicates (imports result)
+			    (if (null imports) result
+			      (let ((f (first imports))
+				    (n (first (rest imports))))
+				(if (or (eclim--java-wildcard-includes-p f n)
+					(equal f n))
+				    (remove-duplicates (cons f (rest (rest imports))) result)
+				  (remove-duplicates (rest imports) (cons f result)))))))
+    (reverse 
+     (remove-duplicates
+      (sort-imports imports-order (sort imports #'string-lessp) '()) '()))))
+
 (defun eclim--java-organize-imports (imports-order)
   "Organize the import statements in the current file."
+  (save-excursion
+    (let ((imports '()))
+      (goto-char 0)
+      (while (search-forward-regexp "^\s*import \\(.*\\);" nil t)
+	(push (match-string-no-properties 1) imports)
+	(beginning-of-line)
+	(kill-line))
+      (delete-blank-lines)
+      (newline)
+      (dolist (import (eclim--java-sort-imports imports imports-order))
+	(insert (format "import %s;\n" import))))))
+
+(defun eclim--java-organize-imports (imports-order)
+
   ;; TODO: this is code I lifted directly from one of my other
   ;; projects. It doesn't care about the specified sort order and
   ;; should probably be replaced.
