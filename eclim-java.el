d5a46a4ec38cb97c4dcaa2abc1f404456d78a511
Tad Fisher <tadfisher@gmail.com>
2012-10-25 13:11:46 -0700

    Replace deprecated java-import* functions with java-import-organize

--- eclim-java.el
+++ eclim-java.el
@@ -35,8 +35,7 @@
 (define-key eclim-mode-map (kbd "C-c C-e f t") 'eclim-java-find-type)
 (define-key eclim-mode-map (kbd "C-c C-e f f") 'eclim-java-find-generic)
 (define-key eclim-mode-map (kbd "C-c C-e r") 'eclim-java-refactor-rename-symbol-at-point)
-(define-key eclim-mode-map (kbd "C-c C-e i") 'eclim-java-import-missing)
-(define-key eclim-mode-map (kbd "C-c C-e u") 'eclim-java-remove-unused-imports)
+(define-key eclim-mode-map (kbd "C-c C-e i") 'eclim-java-import-organize)
 (define-key eclim-mode-map (kbd "C-c C-e h") 'eclim-java-hierarchy)
 (define-key eclim-mode-map (kbd "C-c C-e z") 'eclim-java-implement)
 (define-key eclim-mode-map (kbd "C-c C-e d") 'eclim-java-doc-comment)
@@ -319,97 +318,11 @@ matters for buffers containing non-ASCII characters)."
     (if (re-search-forward "package \\(.*?\\);" (point-max) t)
         (match-string-no-properties 1))))
 
-(defun eclim--java-ignore-import-p (import)
-  "Return true if this IMPORT should be ignored by the import
-  functions."
-  (or (string-match "^java\.lang\.[A-Z][^\.]*$" import)
-      (string-match (concat "^" (eclim--java-current-package) "\.[A-Z][^\.]*$") import)))
-
-(defun eclim--java-sort-imports (imports imports-order)
-  "Sorts a list of imports according to a given sort order,
-removing duplicates."
-  (let* ((non-ordered (loop for a in imports-order
-			    for r = (cdr imports-order) then (cdr r)
-			    while (string< a (car r))
-			    finally return r))
-	 (sorted (make-hash-table)))
-    (loop for imp in (sort imports #'string<)
-	  for key = (or (find imp non-ordered :test #'string-startswith-p)
-			:default)
-	  do (puthash key (cons imp (gethash key sorted)) sorted))
-    (remove-duplicates (loop for key in (cons :default non-ordered)
-			     append (reverse (gethash key sorted)))
-		       :test #'string=)))
-
-(defun eclim--java-extract-imports ()
-  "Extracts (by removing) import statements of a java
-file. Returns a list of the extracted imports. Tries to leave the
-cursor at a suitable point for re-inserting new import statements."
-  (goto-char 0)
-  (let ((imports '()))
-    (while (search-forward-regexp "^\s*import \\(.*\\);" nil t)
-      (unless (save-match-data
-		(string-match "^\s*import\s*static" (match-string 0)))
-	(push (match-string-no-properties 1) imports)
-	(delete-region (line-beginning-position) (line-end-position))
-	(delete-blank-lines)))
-    (if (null imports)
-	  (forward-line))
-    imports))
-
-(defun eclim--java-organize-imports (imports-order &optional additional-imports unused-imports)
-  "Organize the import statements in the current file according
-to IMPORTS-ORDER. If the optional parameter ADDITIONAL-IMPORTS
-is supplied, these import statements will be added to the
-rest. Imports listed in the optional parameter UNUSED-IMPORTS
-will be removed."
-  (save-excursion
-    (flet ((write-imports (imports)
-			  (loop for imp in imports
-				for last-import-first-part = nil then first-part
-				for first-part = (first (eclim--java-package-components imp))
-				do (progn
-				     (unless (equal last-import-first-part first-part)
-				       (newline))
-				     (insert (format "import %s;\n" imp))))))
-      (let ((imports
-	     (remove-if #'eclim--java-ignore-import-p
-			(remove-if (lambda (x) (member x unused-imports))
-				   (append (eclim--java-extract-imports) additional-imports)))))
-	(write-imports (eclim--java-sort-imports imports imports-order))))))
-
-(defun eclim-java-import ()
-  "Reads the token at the point and calls eclim to resolve it to
-a java type that can be imported."
+(defun eclim-java-import-organize ()
+  "Checks the current file for missing imports, removes unused imports and
+sorts import statements. "
   (interactive)
-  (eclim/with-results imports ("java_import" "-n" ("-p" (cdr (eclim--java-identifier-at-point))))
-		      (eclim--java-organize-imports (eclim/execute-command "java_import_order" "-p")
-						    (list (eclim--completing-read "Import: " imports)))))
-
-(defun eclim-java-import-missing ()
-  "Checks the current file for missing imports and prompts the
-user if necessary."
-  (interactive)
-  (eclim/with-results imports-order ("java_import_order" "-p")
-    (loop for unused across (eclim/execute-command "java_import_missing" "-p" "-f")
-			    do (let* ((candidates (append (cdr (assoc 'imports unused)) nil))
-                    (type (cdr (assoc 'type unused)))
-                    (import (if (= 1 (length candidates))
-                                (car candidates)
-                              (eclim--completing-read (concat "Missing type '" type "'")
-                                                      candidates))))
-               (when import
-                 (eclim--java-organize-imports imports-order
-                                               (list (if (string-endswith-p import type)
-                                                         import
-                                                       (concat import "." type)))))))))
-
-(defun eclim-java-remove-unused-imports ()
-  "Remove usused import from the current java source file."
-  (interactive)
-  (eclim/with-results unused ("java_imports_unused" "-p" "-f")
-    (let ((imports-order (eclim/execute-command "java_import_order" "-p")))
-      (eclim--java-organize-imports imports-order nil (append unused '())))))
+  (eclim/execute-command "java_import_organize" "-p" "-f" "-o" "-e"))
 
 (defun eclim-java-implement ()
   "Lets the user select from a list of methods to
