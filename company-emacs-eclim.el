3145bb231a1a71d79f05f4a7b38583bf93ea1a61
James Chochlinski <jchochlinski@xpzen.com>
2015-05-02 08:16:51 -0400

    formatting

--- company-emacs-eclim.el
+++ company-emacs-eclim.el
@@ -60,8 +60,8 @@
   (case command
     (interactive (company-begin-backend 'company-emacs-eclim))
     (prefix (let ((start (and eclim-mode
-			      (eclim--accepted-p (buffer-file-name))
-			      (eclim-completion-start))))
+                              (eclim--accepted-p (buffer-file-name))
+                              (eclim-completion-start))))
               (when start (buffer-substring-no-properties start (point)))))
     (candidates (company-emacs-eclim--candidates arg))
     (annotation (company-emacs-eclim--annotation arg))
