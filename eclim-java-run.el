0f852e76c32c27b9ce1fc968100b87fd0e01c009
Nelson Loyola <nloyola@gmail.com>
2015-12-17 15:31:01 -0700

    Tweak for eclim-run-configuartion.

--- eclim-java-run.el
+++ eclim-java-run.el
@@ -149,16 +149,13 @@
 (defun eclim-run-configuartion (configuration-name)
       "Runs the configuration given in CONFIGURATION-NAME in the compilation buffer."
       (interactive (list (eclim-java-run--ask-which-configuration)))
-      (let* ((current-directory default-directory)
-             (configurations (eclim-java-run--load-configurations (eclim-project-name)))
+      (let* ((configurations (eclim-java-run--load-configurations (eclim-project-name)))
              (configuration (eclim-java-run--configuration configuration-name configurations))
              (project-dir (eclim-java-run--project-dir (eclim-project-name)))
              (classpath (eclim/java-classpath (eclim-project-name)))
+             (default-directory project-dir)
              (command (eclim-java-run--command configuration (eclim-java-run--java-vm-args classpath))))
-        (setq default-directory project-dir)
-        (compile command)
-        (setq default-directory current-directory)
-        ))
+        (compile command)))
 
 (provide 'eclim-java-run)
 ;;; eclim-java-run.el ends here
